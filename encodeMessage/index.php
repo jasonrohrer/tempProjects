c0df4aa191b0aafe6285c8bcba2519472b840d28
Jason Rohrer <jasonrohrer@fastmail.fm>
2012-10-21 11:30:24 -0700

    Close to having encoding script working.

---
+++ encodeMessage/index.php
@@ -0,0 +1,13 @@
+<body bgcolor="#aaaaaa">
+
+English message:<br>
+<FORM ACTION="encodeMessage.php" METHOD="post">
+    <TEXTAREA NAME="message" COLS=90
+          ROWS=20></TEXTAREA>
+<br>
+<br>
+<br>
+	        <INPUT TYPE="Submit" VALUE="generate">
+    </FORM>
+
+</body>
