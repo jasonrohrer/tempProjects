8f4ba19b5ffafcbe297f351c8670d11e7d517510
Jason Rohrer <jasonrohrer@fastmail.fm>
2014-05-22 11:09:34 -0700

    Added magic square test code.

---
+++ magicSquare/magicSquare.cpp
@@ -0,0 +1,180 @@
+
+
+#include <stdio.h>
+
+
+int square[6][6] = {
+    { 6, 32, 3, 34, 35, 1 },
+    { 7, 11, 27, 28, 8, 30 },
+    { 19, 14, 16, 15, 23, 24 },
+    { 18, 20, 22, 21, 17, 13 },
+    { 25, 29, 10, 9, 26, 12 },
+    { 36, 5, 33, 4, 2, 31 }
+    };
+
+
+int count = 0;
+
+void printValues( int *inValues, int inNumValues, void *inUnused ) {
+    for( int i=0; i<inNumValues; i++ ) {
+        printf( "%d ", inValues[i] );
+        }
+    printf( "\n" );
+    count++;
+    }
+
+
+
+void callOnAllPerm( int *inValuesToPermute, int inNumValues, int inNumSkip,
+                    void inProcessPermutationCallback( int *inValues,
+                                                       int inNumValues,
+                                                       void *inExtraParam ),
+                    void *inExtraParam ) {
+
+    if( inNumSkip >= inNumValues ) {
+        inProcessPermutationCallback( inValuesToPermute, inNumValues,
+                                      inExtraParam );
+        return;
+        }
+    
+
+    // pick value to stick in spot at inNumSkip
+    
+    for( int i=inNumSkip; i<inNumValues; i++ ) {
+        
+        int temp = inValuesToPermute[inNumSkip];
+        inValuesToPermute[ inNumSkip ] = inValuesToPermute[i];
+        inValuesToPermute[i] = temp;
+        
+        callOnAllPerm( inValuesToPermute, inNumValues, inNumSkip+1,
+                       inProcessPermutationCallback, inExtraParam );
+        // revert
+        inValuesToPermute[i] = inValuesToPermute[ inNumSkip ];
+        inValuesToPermute[ inNumSkip ] = temp;
+        }
+    }
+
+
+
+typedef struct FixedMove {
+        int *indices;
+        int numIndices;
+    } FixedMove;
+
+
+int playerAWinCount = 0;
+int playerBWinCount = 0;
+int tieCount = 0;
+
+void checkBothPlayerMovesForTie( int *inValues, int inNumValues,
+                                 // pointer to a FixedMove for player A
+                                 void *inPlayerAFixedMove ) {
+
+    FixedMove *m = (FixedMove *)inPlayerAFixedMove;
+    
+    int playerAScore = 0;
+    int playerBScore = 0;
+    
+    for( int i=0; i<inNumValues; i++ ) {
+        int turnScore = square[ m->indices[i] ]
+                              [ inValues[i] ];
+        if( i%2 == 0 ) {
+            playerAScore += turnScore;
+            }
+        else {
+            playerBScore += turnScore;
+            }
+        }
+    
+    if( playerAScore > playerBScore ) {
+        playerAWinCount++;
+        }
+    else if( playerBScore > playerAScore ) {
+        playerBWinCount++;
+        }
+    else {
+        tieCount++;
+
+        /*
+        printf( "Tie:\n" );
+
+        for( int i=0; i<inNumValues; i++ ) {
+            int turnScore = square[ m->indices[i] ]
+                                  [ inValues[i] ];
+            
+            printf( "A picks %d, B picks %d, ",
+                    m->indices[i], inValues[i] );
+
+            if( i%2 == 0 ) {
+                printf( "A scores %d\n", turnScore );
+                }
+            else {
+                printf( "B scores %d\n", turnScore );
+                }
+            }
+        printf( "\n" );
+        */
+        }
+    }
+
+
+
+
+void checkFixedPlayerAMove( int *inValues, int inNumValues, void *inUnused ) {
+    
+    int indices[6] = { 0, 1, 2, 3, 4, 5 };
+    
+    FixedMove m;
+    m.indices = inValues;
+    m.numIndices = inNumValues;
+
+    callOnAllPerm( indices, 6, 0, checkBothPlayerMovesForTie, &m );
+    }
+
+
+
+
+int main() {
+
+    printf( "Test\n" );
+    
+    /*
+    for( int x=0; x<6; x++ ) {
+        int sum = 0;
+        for( int y=0; y<6; y++ ) {
+            sum += square[y][x];
+            }
+        printf( "Sum = %d\n", sum );
+        }
+
+    for( int y=0; y<6; y++ ) {
+        int sum = 0;
+        for( int x=0; x<6; x++ ) {
+            sum += square[y][x];
+            }
+        printf( "Sum = %d\n", sum );
+        }
+    */
+
+    int indices[6] = { 0, 1, 2, 3, 4, 5 };
+    /*
+    count = 0;
+    callOnAllPerm( indices, 6, 0, printValues, NULL );
+
+    printf( "Count = %d\n", count );
+    */
+
+    callOnAllPerm( indices, 6, 0, checkFixedPlayerAMove, NULL );
+
+    int total = playerAWinCount + playerBWinCount + tieCount;
+    
+    printf( "PlayerA Wins %d (%f%%),"
+            "\nPlayer B Wins %d (%f%%),\n"
+            "Tied on %d (%f%%),\n"
+            "Total %d\n\n",
+            playerAWinCount, playerAWinCount * 100.0 / total,
+            playerBWinCount, playerBWinCount * 100.0 / total, 
+            tieCount, tieCount * 100.0 / total, total );
+    
+    return 1;
+    }
