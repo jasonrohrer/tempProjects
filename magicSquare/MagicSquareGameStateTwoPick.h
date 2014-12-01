4cc3ddf7df9ca16d06624f67f917cf094691408b
Jason Rohrer <jasonrohrer@fastmail.fm>
2014-12-01 09:18:23 -0800

    Added test of minmax AI as player 1 vs peeking, single-turn-greedy player 2, where each player picks two rows or columns as a single move.

---
+++ magicSquare/MagicSquareGameStateTwoPick.h
@@ -0,0 +1,63 @@
+#include "minorGems/ai/gameTree/GameState.h"
+
+
+
+// game played on 6x6 magic squares
+// player 1 picks row, player 2 picks column
+//
+// This version has players make both choices in one turn
+class MagicSquareGameStateTwoPick : public GameState {
+    public:
+        
+        // blank, no init done
+        MagicSquareGameStateTwoPick();
+        
+        // square copied internally
+        // inits to no moves made yet
+        MagicSquareGameStateTwoPick( int *inStartingState );
+
+
+        // get separate, positive score for each player
+        // (different than min-max score, where players push score positive
+        // or negative)
+        int getScore( int inPlayerNumber );
+        
+        // total number of moves made by both players
+        int getNumMovesMade();
+        
+        
+        GameState *makeMove( int inPlayerNumber, int inColumnOrRowUs );
+
+        // flips square and player moves made so far, turning rows into columns
+        // p1 becomes p2, etc.
+        GameState *flipGame();
+        
+
+
+        // relative state score for purpose of minMax search
+        // (player 1 pushes score positively, player 2 negatively)
+        virtual int getScore( char inDebug=false );
+        
+
+        virtual char getGameOver();
+
+        virtual SimpleVector<GameState *> getPossibleMoves();
+        
+
+        virtual GameState *copy();
+        
+        virtual void copyFrom( GameState *inOther );
+
+        virtual void printState();
+        
+
+
+
+        int mSquare[6][6];
+        
+        // -1 if not taken yet, otherwise show row or column that player
+        // picked on that move
+        int mPlayerMoves[2][6];
+
+        char mScoringFlipped;
+    };
