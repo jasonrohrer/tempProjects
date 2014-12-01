ece626ac1d31dd18b4065e8fb21c6fdcd06ab84d
Jason Rohrer <jasonrohrer@fastmail.fm>
2014-12-01 09:45:19 -0800

    Added full tree search for any forced win against one-turn-peeking greedy.  Found 3/100 magic squares where this is possible.

--- magicSquare/greedyPeekingTest.cpp
+++ magicSquare/greedyPeekingTest.cpp
@@ -129,11 +129,101 @@ static void runMinMaxVsPeekingGreedy( int inSquareSeed ) {
 
 
 
+// max player (p1) searches all possible moves
+// min player (p2) considers only single-turn greedy move
+// returns highest seen score
+static int vsGreedyTreeSearch( GameState *inState, MinOrMax inNext ) {
+    if( inState->getGameOver() ) {
+        if( inState->getScore() > 0 ) {
+            printf( "Found forced positive score vs. greedy: %d\n",
+                    inState->getScore() );
+            inState->printState();
+            }
+        return inState->getScore();
+        }
+    
+    if( inNext == max ) {
+        SimpleVector<GameState *> possibleMoves = 
+            inState->getPossibleMoves();
+
+        int highScore = -200;
+        
+        for( int i=0; i<possibleMoves.size(); i++ ) {
+            GameState *move = possibleMoves.getElementDirect( i );
+            
+            int score = vsGreedyTreeSearch( move, min );
+            
+            if( score > highScore ) {
+                highScore = score;
+                }
+            delete move;
+            }
+        return highScore;
+        }
+    else {
+        int minScore = 200;
+        
+        GameState *minScoreMove = NULL;
+        
+        SimpleVector<GameState *> possibleMoves = 
+            inState->getPossibleMoves();
+
+        for( int i=0; i<possibleMoves.size(); i++ ) {
+            GameState *move = possibleMoves.getElementDirect( i );
+            
+            int score = move->getScore();
+            
+            if( score < minScore ) {
+                minScore = score;
+                
+                if( minScoreMove != NULL ) {
+                    delete minScoreMove;
+                    }
+                minScoreMove = move->copy();    
+                }
+            delete move;
+            }
+        int score = vsGreedyTreeSearch( minScoreMove, max );
+        
+        delete minScoreMove;
+        
+        return score;
+        }
+    }
+
+
+static void runVsGreedyTreeSearch( int inSquareSeed ) {
+    
+
+    int *squareA = generateMagicSquare6( 10 + inSquareSeed );
+
+    //printSquare( squareA, 6 );
+
+    MagicSquareGameStateTwoPick state( squareA );
+
+    int highScore = vsGreedyTreeSearch( &state, max );
+    printf( "Highest score = %d\n", highScore );
+    
+    if( highScore > 0 ) {
+        printSquare( squareA, 6 );
+        
+        int score = minMax( &state, max );
+    
+        printf( "Best worst-case score (minmax vs minmax) = %d\n", score );
+
+        }
+    
+    delete [] squareA;
+    }
+
+
+
 int main() {
 
     
-    for( int r=0; r<1; r++ ) {
+    for( int r=0; r<100; r++ ) {
 
-        runMinMaxVsPeekingGreedy( r );
+        //runMinMaxVsPeekingGreedy( r );
+        runVsGreedyTreeSearch( r );
         }
     }
