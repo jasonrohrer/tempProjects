ec0e116c18b7b47918f595c4af17ac67cb44a2f2
Jason Rohrer (office) <Jason Rohrer (office)@localhost>
2017-06-16 11:33:48 -0700

    Updated cron job to generate nightly for games too.

--- metascoreStdDev/updateMetascores.sh
+++ metascoreStdDev/updateMetascores.sh
@@ -2,3 +2,8 @@ php -f metascoreStdDev.php > www/metascoreStdDev_temp.html
 
 mv www/metascoreStdDev_temp.html www/metascoreStdDev.html
 
+
+php -f metascoreGames90StdDev.php > www/metascoreGames90StdDev_temp.html
+
+mv www/metascoreGames90StdDev_temp.html www/metascoreGames90StdDev.html
+
