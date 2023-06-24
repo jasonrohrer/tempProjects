36ecd25aaacb5b0f0222b5804bcc32efc383a61a
Jason Rohrer <jasonrohrer@fastmail.fm>
2023-06-23 21:29:32 -0400

    Working on puppeteer script to fetch surfline with chome.

---
+++ surfline/fetchJsonURL.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const fsp = require('fs').promises;
+
+if( process.argv.length < 3 ) {
+	console.log( "Missing URL arg" );
+	process.exit();
+}
+
+const url = process.argv[2];
+
+(async () => {
+
+	// use system-installed browser (one installed by puppeteer has
+	// dependency issues)
+	const browser = await puppeteer.launch(
+		{ executablePath:'/usr/bin/chromium-browser' });
+
+	const page = await browser.newPage();
+
+	// Set screen size
+	await page.setViewport({width: 1280, height: 720});
+
+	// bypass allow-cookie confirmation dialog box
+	await page.setCookie( { 'name': 'necessaryCookiesAllowed',
+							'value': 'yes',
+							'domain': 'login.reconyx.com' } );
+	await page.setCookie( { 'name': 'necessaryCookiesAllowed',
+							'value': 'yes',
+							'domain': 'connect.reconyx.com' } );
+	
+	
+	var response = await page.goto( url ,
+									{ waitUntil: 'networkidle2' } );
+	
+	const text = await response.text();
+	
+	process.stdout.write( text );
+};
