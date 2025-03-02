1d9a47ad642e197f69d1ccbc509dff3d1be6d024
Jason Rohrer <jasonrohrer@fastmail.fm>
2025-03-02 12:35:46 -0500

    By splitting code into separate functions, we get a compilation speed-up due to multithreaded compilation.

--- compSpeedTest/makeCodeNoFunctions_jai.c
+++ compSpeedTest/makeCodeNoFunctions_jai.c
@@ -3,21 +3,37 @@
 
 int main() {
 
-    int numLoops = 160000;
-    int j;
+    int numLoops = 10000;
+    int numFunctions = 16;
+    
+    int j, f;
 
+    srand( 420 );
+    
     printf( "#import \"Basic\";\n\n" );
+
+
+    for( f=0; f<numFunctions; f++ ) {
+        printf( "function_%d :: () -> int {\n\n", f );
+        printf( "x : int = 0;\n\n" );
+
+        for( j=0; j<numLoops; j++ ) {
+            printf( "x = x + %d;\n\n", (int)( 10 * (float)rand() / RAND_MAX ) );
+            }
+        printf( "return x;\n\n}\n\n" );
+        }
+    
+    
     
     
     printf( "main :: () {\n\n" );
 
     printf( "x : int = 0;\n\n" );
 
-    srand( 420 );
-    
-    for( j=0; j<numLoops; j++ ) {
-        printf( "x = x + %d;\n\n", (int)( 10 * (float)rand() / RAND_MAX ) );
+    for( f=0; f<numFunctions; f++ ) {
+        printf( "x = x + function_%d();\n\n", f );
         }
+    
     
     printf( "print( \"Result = %%\\n\", x );" );
     printf( "\n\n}\n" );
