5d36eed161623e11c29eab2e95184cccd334d334
Jason Rohrer <jasonrohrer@fastmail.fm>
2025-03-04 16:36:32 -0500

    Info duiring build, and correct exe name for jai build.

--- compSpeedTest/blurTest/buildBlur.sh
+++ compSpeedTest/blurTest/buildBlur.sh
@@ -1,11 +1,22 @@
-time ~/Downloads/jai/bin/jai-linux -release blurPNG.jai
+echo
+echo "Building with jai"
 
+time ~/Downloads/jai/bin/jai-linux -exe blurPNG_jai -release blurPNG.jai
+
+
+echo
+echo "Building with gcc"
 
 time gcc -O3 -o blurPNG_c blurPNG.c -lm
 
 
+echo
+echo "Building with clang"
 time clang -O3 -o blurPNG_clang blurPNG.c -lm
 
 
+
+echo
+echo "Building with odin"
 
 time ~/Downloads/odin/odin-linux-amd64-dev-2025-02/odin build . -out=./blurPNG_odin -o:speed -no-bounds-check
