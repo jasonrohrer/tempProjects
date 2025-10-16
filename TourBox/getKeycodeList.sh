e1aeb1b5faf24a410ae180dfe06300213f3e9256
Jason Rohrer <jasonrohrer@fastmail.fm>
2025-10-16 16:49:47 -0400

    Script now generates code array.

--- TourBox/getKeycodeList.sh
+++ TourBox/getKeycodeList.sh
@@ -1,1 +1,18 @@
-cat /usr/include/linux/input.h | grep "#define KEY_" | sed "s/\t.*//g" | sed "s/#define //"
+cat /usr/include/linux/input.h | grep "#define KEY_" | sed "s/\t.*//g" | sed "s/#define //" > keyCodeList.txt
+
+
+cat keyCodeList.txt | sed "s/\([A-Z_]*\)/\1,/" > codeArrayTemp.txt
+
+numCodes="$( wc -l keyCodeList.txt | sed 's/ .*//' )"
+
+echo "Num codes = $numCodes"
+
+
+printf "#define NUM_KEY_CODES 403\n\n" > codeArray.txt
+
+printf "int keyCodes[NUM_KEY_CODES] = {\n" >> codeArray.txt
+
+# remove final comma
+head -c-2 codeArrayTemp.txt >> codeArray.txt
+
+echo " };" >> codeArray.txt
